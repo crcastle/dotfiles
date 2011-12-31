bcf40bc91dd03c4ef0c33c4e460854fa1d14013e
crcastle <crcastle@gmail.com>
2011-12-30 17:54:41 -0800

    shortcut for 'git' and some git-svn shortcuts

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,4 @@
+alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -8,3 +9,7 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+# git svn junk
+alias gsf="git svn fetch"
+alias gsr="git svn rebase"
