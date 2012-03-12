68e982d022d59a2d55f18c026585259b6c9b302a
Chris Castle <crcastle@gmail.com>
2012-03-12 14:01:06 -0700

    Notational Velocity todo manager shortcut

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,6 @@
 alias reload!='. ~/.zshrc'
+
+# TODOS
+# This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
+# to find the note called 'todo'
+alias todo='open nvalt://find/todo'
