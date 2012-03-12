fb47b66bda65dfaebc9fcca77c7c6665c185e86b
Chris Castle <crcastle@gmail.com>
2012-03-12 14:02:48 -0700

    Give me my bash style incremental search!

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -46,3 +46,6 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+# Give me my bash style incremental search
+bindkey '^R' history-incremental-search-backward
