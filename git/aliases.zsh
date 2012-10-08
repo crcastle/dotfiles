482d533d7301ac63a3710405abe35a041b719b04
Chris Castle <crcastle@gmail.com>
2012-10-07 22:09:05 -0700

    Shortcuts to see unpulled and unpushed commits

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,6 +9,9 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gunpushed="git log origin/$(current_branch)..HEAD"
+alias gunpulled="git log HEAD..origin/$(current_branch)"
+alias unpulled='gunpulled'
 
 # git svn junk
 alias gsf="git svn fetch"
