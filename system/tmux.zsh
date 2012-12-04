6b43021e5460f0b74e8cfa6914c0232f832d36c9
Chris Castle <crcastle@gmail.com>
2012-12-03 20:12:28 -0800

    Change terminal background on SSH

--- system/tmux.zsh
+++ system/tmux.zsh
@@ -8,8 +8,26 @@ settitle() {
     printf "\033k$1\033\\"
 }
 
-ssh() {
-    settitle "$*"
-    command ssh "$@"
+set_bg() {
+  osascript -e "tell application \"iTerm\"
+    set current_terminal to (current terminal)
+    tell current_terminal
+      set current_session to (current session)
+      tell current_session
+        set background image path to \"$1\"
+      end tell
+    end tell
+  end tell"
+}
+
+on_exit() {
+    set_bg
     settitle $(hostname -s)
+}
+
+ssh() {
+  settitle $(echo $* | sed "s/.*@//" | sed "s/ .*//")
+  set_bg "/Users/crcastle/Dropbox/Photos/Wallpapers/bear.png"
+  trap on_exit EXIT
+  command ssh "$@"
 }
