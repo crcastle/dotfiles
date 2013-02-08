7427611005906fa4da13644aef8c078feeca5a57
Chris Castle <crcastle@gmail.com>
2013-02-07 19:56:04 -0800

    SourceTree wanted to make some changes here. I let him.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -116,6 +116,9 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
+# Save screenshots to a specified folder
+defaults write com.apple.screencapture location "~/Desktop/Screenshots"
+
 # Disable shadow in screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
 
