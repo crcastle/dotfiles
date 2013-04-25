7e0077aaada8ef67b9fd983b6b10ec665b0aa08d
Chris Castle <crcastle@gmail.com>
2013-04-24 21:49:18 -0700

    Fix setting screenshot location twice

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -110,9 +110,6 @@ defaults write com.apple.BezelServices kDimTime -int 300
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "$HOME/Desktop"
-
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
