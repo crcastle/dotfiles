6f3ce7ae256e069672110ff48ba8884fe41bcdc5
Chris Castle <crcastle@gmail.com>
2013-08-20 18:02:38 -0700

    Adds GOPATH env var and updates PATH for Go

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$DOT/bin:$HOME/bin:$HOME/local/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$DOT/bin:$HOME/bin:$HOME/local/bin:$GOPATH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
