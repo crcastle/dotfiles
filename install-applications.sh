4d33b4c8dbcdef444c51f560a99290f9a61f0c89
Chris Castle <crcastle@gmail.com>
2013-08-21 23:27:07 -0700

    Adds automated application install

---
+++ install-applications.sh
@@ -0,0 +1,39 @@
+# Installs the applications I want on my mac
+#
+# Assumes brew is installed
+#
+# Run ./install-applications.sh
+
+# Add the cask homebrew plugin if not already installed
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# Install ALL THE APPZ!
+brew cask install air-server
+brew cask install airfoil
+brew cask install alfred
+brew cask install arq
+brew cask install bartender
+brew cask install bit-torrent-sync
+brew cask install caffeine
+brew cask install cloudapp
+brew cask install cord
+brew cask install cyberduck
+brew cask install divvy
+brew cask install dropbox
+brew cask install f-lux
+brew cask install firefox
+brew cask install google-chrome
+brew cask install handbrake
+brew cask install istats-menus
+brew cask install iterm2
+brew cask install kaleidoscope
+brew cask install kindle
+brew cask install mou
+brew cask install mplayerx
+brew cask install nv-alt
+brew cask install sublime-text
+brew cask install transmission
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
