29396c42f8a32cec287b1081ec43cf8ba465b17d
Chris Castle <crcastle@gmail.com>
2014-04-26 18:02:40 -0700

    Use gnu coreutils

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,13 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-#if $(gls &>/dev/null)
-#then
-#  alias ls="gls -F --color"
-#  alias l="gls -lAh --color"
-#  alias ll="gls -l --color"
-#  alias la='gls -A --color'
-#fi
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
+alias man='_() { echo $1; man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1 1>/dev/null 2>&1;  if [ "$?" -eq 0 ]; then man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1; else man $1; fi }; _'
+
+
