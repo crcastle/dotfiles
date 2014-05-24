13c4203924ee0877dc131dfe762aeb776a0e0cbe
Chris Castle <crcastle@gmail.com>
2014-05-23 21:28:10 -0700

    remove crazy dumb hack for gnu man pages

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,6 +8,4 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
-alias man='_() { echo $1; man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1 1>/dev/null 2>&1;  if [ "$?" -eq 0 ]; then man -M $(brew --prefix)/opt/coreutils/libexec/gnuman $1; else man $1; fi }; _'
-
 
