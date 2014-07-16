a72a040713fc4cb487386ef1bd0970b4d185bd3f
Chris Castle <crcastle@gmail.com>
2014-07-16 14:22:59 -0700

    Make redis-up send stdout and stderr to /dev/null

--- redis/aliases.zsh
+++ redis/aliases.zsh
@@ -1,2 +1,2 @@
-alias redis-up='redis-server /usr/local/etc/redis.conf > /dev/null &'
 alias redis-down='killall redis-server'
+alias redis-up='redis-server /usr/local/etc/redis.conf > /dev/null 2>&1 &'
